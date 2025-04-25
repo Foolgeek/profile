dcd6f78acbd9e43f1bd657b0f2a3f4e04ae27715
Foolgeek <lightingrider5@gmail.com>
2025-04-25 17:29:04 +0545

    googleform

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('openPopup').onclick = function() {
+    document.getElementById('popup').style.display = 'flex';
+  };
+  
+  document.getElementById('closePopup').onclick = function() {
+    document.getElementById('popup').style.display = 'none';
+  };
+  
